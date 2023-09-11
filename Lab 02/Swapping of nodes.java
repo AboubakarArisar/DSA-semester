adb8e4ee25976ec6e8dc0f34c4dcfed8f95dab2c
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-09-11 12:34:07 +0500

    Swapping of nodes.java

---
+++ Lab 02/Swapping of nodes.java
@@ -0,0 +1,81 @@
+
+class LinkedList {
+  class Node {
+    Node next;
+    int data;
+
+    Node(int data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  Node head = null;
+
+  void add(int item) {
+    Node node = new Node(item);
+    if (head == null) {
+      head = node;
+    } else {
+      Node curr = head;
+      node.next = curr;
+      head = node;
+    }
+  }
+
+  void swap(int M, int N) {
+    if (M == N) {
+      return; // No need to swap if M and N are the same
+    }
+
+    // Find the nodes to be swapped
+    Node prevM = null, currM = head;
+    while (currM != null && currM.data != M) {
+      prevM = currM;
+      currM = currM.next;
+    }
+
+    Node prevN = null, currN = head;
+    while (currN != null && currN.data != N) {
+      prevN = currN;
+      currN = currN.next;
+    }
+
+    // Update next pointers to swap nodes
+    if (prevM != null) {
+      prevM.next = currN;
+    } else {
+      head = currN;
+    }
+
+    if (prevN != null) {
+      prevN.next = currM;
+    } else {
+      head = currM;
+    }
+
+    Node temp = currM.next;
+    currM.next = currN.next;
+    currN.next = temp;
+  }
+
+  void print() {
+    Node curr = head;
+    while (curr != null) {
+      System.out.print(curr.data + " ");
+      curr = curr.next;
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    LinkedList ll = new LinkedList();
+    ll.add(1);
+    ll.add(2);
+    ll.add(3);
+    ll.add(4);
+    ll.add(5);
+    ll.swap(2, 4); //passing data of nodes that we want to sort
+    ll.print();
+  }
+}
