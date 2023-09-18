5311b0d72df40e4cb1e519ac6f44aaafb34bab76
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-09-18 13:26:54 +0500

    Task2.java
    fabonacii series using loop

---
+++ Lab 05/Task2.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+public class Task2 {
+    public static void printSeries(int range) {
+         int sum = 0 , next = 1, curr;
+         System.out.print(sum + " ");
+         for(int i = 0 ; i <= range; i++){
+            curr=next;
+            next=sum;
+            sum=curr+next;
+            System.out.print(sum+" ");
+         }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the range till you want fibonacci series --> ");
+        int range = sc.nextInt();
+        long startTime = System.currentTimeMillis();
+        System.out.println("======Fabonacci Series======");
+        printSeries(range);
+        System.out.println();
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time taken in iterative approach is --> "+(startTime - endTime) + "ms");
+    }
+}
