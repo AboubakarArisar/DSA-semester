ee9ead2f2a0068f34c90758fd494a13329a19f77
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-09-23 11:22:25 +0500

    Task1b.java

--- Lab 05/Task1b.java
+++ Lab 05/Task1b.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class Task2b {
+public class Task1b {
     public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
@@ -15,13 +15,13 @@ public class Task2b {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the range -->  ");
         int N = scanner.nextInt();
-        long startTime = System.currentTimeMillis();
+        long startTime = System.nanoTime();
         System.out.println("Fibonacci series up to range ");
         for (int i = 0; i < N; i++) {
             System.out.print(fibonacci(i) + " ");
         }
         System.out.println();
-        long endTime = System.currentTimeMillis();
+        long endTime = System.nanoTime();
         System.out.println("Time taken in recursive approach: " + (endTime - startTime) + " ms");
     }
 
