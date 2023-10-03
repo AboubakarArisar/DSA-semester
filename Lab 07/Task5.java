2bda101952e61e650191c52101464315c8663538
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-10-03 14:24:05 +0500

    Task5.java

---
+++ Lab 07/Task5.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
