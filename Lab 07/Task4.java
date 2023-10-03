0c9b83e9ca0d2cd77ee8c1d1cbe7bc3668be3cb2
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-10-03 13:44:59 +0500

    Task4.java

---
+++ Lab 07/Task4.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] expected = Arrays.copyOf(heights, heights.length);
+        Arrays.sort(expected);
+        
+        int count = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != expected[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
